df838f480b18c47f3397158d73094a6e9f78d214
Lexie <lexie@Lexies-MacBook-Pro.local>
2019-04-06 06:00:31 -0400

    Update the BoardStatus class

--- Programs/lazor_board.py
+++ Programs/lazor_board.py
@@ -3,7 +3,7 @@
 """
 Created on Thu Mar 28 23:41:11 2019
 
-@author: Wenye Deng and Yuchun Wang
+@author: Wenye Deng, Yuchun Wang
 """
 
 import numpy as np
@@ -14,87 +14,144 @@ class BoardStatus():
     """
     def __init__(self, grid, lasers, blocks, points):
         self.grid = grid
-        self.laser = lasers
-        self.block = blocks
-        self.point = points
+        self.lasers = lasers
+        self.blocks = blocks
+        self.points = points
 
-    # def cood_trans(self):
-    #     '''
-    #     
-    #     '''
-    #     pass
-
-    def reflect(self):
-        pass
 
         
     def laser_pathway(self):
         """
-        A function calculate the points that laser will pass through.
+        A function calculate all the points that laser will pass through on the current status.
         """
+        def coord_trans(p1, p2):
+            '''
+            Transfer point coordinates into row and column of grid
+            
+            **Parameteres**
+            
+                p1: *array, int*
+                    Coordinate of first point.
+                p2: *array, int*
+                    Coordinate of a neighbor point.
+                    
+            **Returns**
+            
+                pos: *list, int*
+                    The position both p1 and p2 occupied.
+            '''
+            position = []
+            
+            if p1[0] % 2 == 0:
+                position.append((p1[1]-1) / 2)
+                position.append((p2[0]-1) / 2)
+            else:
+                position.append((p2[1]-1) / 2)
+                position.append((p1[0]-1) / 2)
+                
+            return position
+
         x_bound = 2 * len(self.grid[0])
         y_bound = 2 * len(self.grid)
         
-        all_laser_path = []
-        for l in self.laser:
+        laser_path = []
+        for l in self.lasers:
             
-            laser_point = np.array([int(l[0]), int(l[1])]) + np.array([int(l[2]), int(l[3])])
-            laser_path = [laser_point]
-            
-            while True:
-                laser_point = laser_point + np.array([int(l[2]), int(l[3])])
+            start_point = np.array([l[0], l[1]])
+            laser_path.append(start_point)
+            laser_point = start_point
+            current_point = start_point
+            previous_point = np.array([0, 0])
+                        
+            while not previous_point.all == current_point.all:
+                previous_point = current_point                
+                laser_point = current_point + np.array([l[2], l[3]])
                 if laser_point[0] <= x_bound and laser_point[0] >= 0 and laser_point[1] <= y_bound and laser_point[1] >= 0:
-                    laser_path.append(laser_point)
+                    # Check if there any block on next move
+                    pos = coord_trans(current_point, laser_point)
+                    if self.grid[pos[0]][pos[1]] == 'o':
+                        laser_path.append(laser_point)
+                        current_point = laser_point
+                    # Change the direction of the laser
+                    elif self.grid[pos[0]][pos[1]] == 'A':
+                        if current_point[0] % 2 == 0:
+                            self.lasers.append([current_point[0], current_point[1], -l[2], l[3]])
+                        else: 
+                            self.lasers.append([current_point[0], current_point[1], l[2], -l[3]])
+                    elif self.grid[pos[0]][pos[1]] == 'C':
+                        # Refracted lazer
+                        self.lasers.append([laser_point[0], laser_point[1], l[2], l[3]])
+                        # Reflected lazer
+                        if current_point[0] % 2 == 0:
+                            self.lasers.append([current_point[0], current_point[1], -l[2], l[3]])
+                        else: 
+                            self.lasers.append([current_point[0], current_point[1], l[2], -l[3]])
+                    else:
+                        break
                 else:
-                    break
-            all_laser_path.append(laser_path)
+                    break               
         
-        return all_laser_path
+        return laser_path
     
-    # def possible_positions(self):
-    #     pass
+     
+    def put_block(self, block_pos, block_type):
+        '''
+        A function that put block on a specific position of the board.
+        
+        **Parameters**
+        
+            block_pos: *list, int*
+                The position on the board want to put block.
+            block_type: *str*
+                One of three different types of block.
+                Possible value are 'A', 'B' or 'C'.
+                
+        **Returns**
+        
+            self.grid: *list*
+                A matrix describe the board.
+            self.blocks: *dict*
+                The updated blocks set after puting block on board.
+        '''
+        
+        self.blocks[block_type] -= 1
+        if self.blocks[block_type] == 0:
+           del self.blocks[block_type] 
+        self.grid[block_pos[0]][block_pos[1]] = block_type
+        return self.grid, self.blocks
+            
     
-    def replace(self, grid, m, n, c):
-    	# Replace the item in specific position [m,n] of grid with new item c
-    	new = []
-    	string = self.grid[m]
-    	for s in string:
-    		new.append(s)
-    	new[n] = c
-    	self.grid[m] = ''.join(new)
-    	return self.grid
-
-    def put_block(self, block_position, block_type):
-        # Put a specific block into  sepecific position []
-        self.grid = replace(self.grid, block_position[0] - 1, (block_position[1] - 1) * 4, block_type)
-        block_number = int(self.block[block_type]) - 1
-        self.block[block_type] = str(block_number)
-        if block_number == 0:
-        	del self.block[block_type]
-        return self.grid, self.block
-    
-    def delete_block(self, block_position):
-        # Delete a block in sepecific position []
-        m = block_position[0] - 1
-        n = (block_position[1] - 1) * 4
-        block_type = self.grid[m][n]
-        self.grid = replace(self.grid, m, n, 'o')
-        if block_type in self.block:
-        	block_number = int(self.block[block_type]) + 1
-        	self.block[block_type] = str(block_number)
+    def delete_block(self, block_pos, block_type):
+        '''
+        A function that delete block on a specific position of the board.
+        
+        **Parameters**
+        
+            block_pos: *list, int*
+                The position on the board want to delete block.
+            block_type: *str*
+                One of three different types of block.
+                Possible value are 'A', 'B' or 'C'.
+                
+        **Returns**
+        
+            self.grid: *list*
+                A matrix describe the board.
+            self.blocks: *dict*
+                The updated blocks set after deleting block on board.
+        '''
+        if block_type not in self.blocks:
+            self.blocks[block_type] = 1
         else:
-        	self.block[block_type] = str(1)
-        return self.grid, self.block
+            self.blocks[block_type] += 1
+        self.grid[block_pos[0]][block_pos[1]] = 'o'
+        return self.grid, self.blocks
 
+        
     
 class TreeNode():
     def __init__(self, board_status):
         self.board_status = board_status
-        self.parent = bs
-        self.children = []
-        
-        for (i, elem) in enumerate(possible_step):
-            locals()['children' + str(i)] = elem
 
     
 class GameTree():

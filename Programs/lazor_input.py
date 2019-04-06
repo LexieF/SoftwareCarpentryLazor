f1b73947644f3fa5e66c05c15e88774e16af051a
Lexie <lexie@Lexies-MacBook-Pro.local>
2019-04-06 01:39:04 -0400

    transfer some str output into int

--- Programs/lazor_input.py
+++ Programs/lazor_input.py
@@ -3,12 +3,15 @@
 """
 Created on Tue Mar 26 22:14:27 2019
 
-@author: lexie
+@author: Wenye Deng
 """
 
-filename = "../Lazor_board/mad_1.bff"
+
 
 def read_lazor_board(filename):
+    """
+    
+    """
     f = open(filename)
     l = f.readlines()
     f.close()
@@ -30,26 +33,27 @@ def read_lazor_board(filename):
             s = True # s means Grid Start or Stop
             i = 1
             while s:
-                grid.append(l[l_ind+i].split('\n')[0])
+                temp_str = l[l_ind+i].split('\n')[0]
+                grid.append(temp_str.split('   '))
                 i += 1
                 if l[l_ind+i][0] == "G":
                     l_ind += i+1
                     s = False
                     break
-    
+        # Read blocks
         elif l[l_ind][0] == "A" or l[l_ind][0] == "B" or l[l_ind][0] == "C":
             temp_str = l[l_ind].split('\n')[0]
-            blocks[temp_str.split(' ')[0]] = temp_str.split(' ')[1]
+            blocks[temp_str.split(' ')[0]] = int(temp_str.split(' ')[1])
             l_ind += 1
         
         elif l[l_ind][0] == "L":
             temp_str = l[l_ind].split('\n')[0]
-            lasers.append(temp_str.split(' ')[1:5])
+            lasers.append(map(int, temp_str.split(' ')[1:5]))
             l_ind += 1
         
         elif l[l_ind][0] == "P":
             temp_str = l[l_ind].split('\n')[0]
-            points.append([temp_str.split(' ')[1:3]])
+            points.append(map(int, temp_str.split(' ')[1:3]))
             l_ind += 1
         
         else:
@@ -57,6 +61,7 @@ def read_lazor_board(filename):
             
     return grid, lasers, blocks, points
 
+# Test the read_lazor_board() function
 if __name__ == "__main__":
     filename_list = ["dark_1", "mad_1", "mad_4", "mad_7", "numbered_6", "showstopper_4", "tiny_5", "yarn_5"]
     for f in filename_list:

3b4561d61d35fa7148fb4b65b7d0b856d34c2ef8
jmsgolding <47226552+jmsgolding@users.noreply.github.com>
2019-04-08 15:07:48 -0400

    Create check.py

---
+++ Programs/check.py
@@ -0,0 +1,28 @@
+'''
+Made by Joan Golding 
+4/8/19 
+
+This function serves to check if 
+our game tree was able to correctly
+solve the Lazor board. 
+
+***Inputs***
+	laser_path = path of the laser from previous function
+	tree function = our function to check moves 
+
+**Outputs**
+	Boolean: True if the moves can solve the board
+			 False if they cannot solve the board
+
+'''
+import laser_pathway
+def check(board_state):
+	# check the beam of the lazor
+	# check if the destination point is on the beam of the lazor
+	# if the beam meets all the required points, return True
+	# if not, return False and it goes back to the 
+	# expansion function 
+
+	for i in laser_pathway:
+    if i = points:
+    return True 
